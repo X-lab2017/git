a34979f113887133e8bbf6a21f4d31c41fb8ec0d
guodong <gd@tongji.edu.cn>
2019-04-16 14:14:47 +0800

    preload

---
+++ scripts/preload.js
@@ -0,0 +1,15 @@
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+fetch('http://api.kfcoding.com/api/practice/trains/competition/begin', {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: getCookie('token')
+  },
+  method: 'POST',
+  body: JSON.stringify({repo: 'https://github.com/openx-lab/git.git'})
+}).then(resp => resp.json())
+  .then(data => localStorage.setItem('comp_id', data.data.id));
