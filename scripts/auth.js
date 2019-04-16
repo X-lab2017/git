17da6586cf2ecbe3fa4f918d3fabe2406ae5fd26
guodong <gd@tongji.edu.cn>
2019-04-16 17:49:35 +0800

    host diff

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,16 +4,18 @@ function getCookie(name) {
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
-fetch('http://api.kfcoding.com/api/basic/users/current', {
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: getCookie('token')
-  },
-  method: 'GET',
-}).then(resp => resp.json())
-  .then(data => {
-    if (data.status !== 200){
-      window.location.href = "http://kfcoding.com/user/login?redirect="+ window.location.href;
-    }
-  });
+if (document.location.host == 'gitcourse.kfcoding.com') {
 
+  fetch('http://api.kfcoding.com/api/basic/users/current', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: getCookie('token')
+    },
+    method: 'GET',
+  }).then(resp => resp.json())
+    .then(data => {
+      if (data.status !== 200) {
+        window.location.href = "http://kfcoding.com/user/login?redirect=" + window.location.href;
+      }
+    });
+}
