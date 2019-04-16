473a702a4ccf085fa49d15c700a67a8bf598e4dc
guodong <gd@tongji.edu.cn>
2019-04-16 15:39:46 +0800

    auth

---
+++ scripts/auth.js
@@ -0,0 +1,19 @@
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+fetch('http://api.kfcoding.com/api/basic/users/current', {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: getCookie('token')
+  },
+  method: 'GET',
+}).then(resp => resp.json())
+  .then(data => {
+    if (data.status !== 200){
+      window.location.href = "http://kfcoding.com/user/login?redirect="+ window.location.href;
+    }
+  });
+
