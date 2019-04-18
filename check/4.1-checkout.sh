34dacda500474d29c6ce40aff5f14dc6a3d14cef
liwen-tj <liwen00812@qq.com>
2019-04-18 14:45:27 +0800

    Update 4.1-checkout.sh

--- check/4.1-checkout.sh
+++ check/4.1-checkout.sh
@@ -1,6 +1,5 @@
 cd /gitscenario4
-output=$(git status | grep add | wc -l)
-if [ $output = "2" ]
-then
-    echo 1
+output=$(git status | grep update | wc -l)
+if [ $output -eq "1" ];then
+echo 1
 fi
