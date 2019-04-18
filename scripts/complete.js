5763cf14b62ba1da4d68d41232ba80633a1d49d1
guodong <gd@tongji.edu.cn>
2019-04-18 14:14:14 +0800

    fix

--- scripts/complete.js
+++ scripts/complete.js
@@ -3,24 +3,26 @@ function getCookie(name) {
   var parts = value.split("; " + name + "=");
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
-
-fetch('http://api.kfcoding.com/api/basic/practice/trains/competition', {
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: getCookie('token')
-  },
-  method: 'POST',
-  body: JSON.stringify({repo: 'https://github.com/openx-lab/git.git'})
-}).then(resp => resp.json())
-  .then(data => {
-    return fetch('http://api.kfcoding.com/api/practice/trains/competition/end', {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: getCookie('token')
-      },
-      method: 'PUT',
-      body: JSON.stringify({id: data.data.id})
+try {
+  fetch('http://api.kfcoding.com/api/basic/practice/trains/competition', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: getCookie('token')
+    },
+    method: 'POST',
+    body: JSON.stringify({repo: 'https://github.com/openx-lab/git.git'})
+  }).then(resp => resp.json())
+    .then(data => {
+      return fetch('http://api.kfcoding.com/api/practice/trains/competition/end', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: getCookie('token')
+        },
+        method: 'PUT',
+        body: JSON.stringify({id: data.data.id})
+      })
     })
-  })
-
+} catch (e) {
+  console.log(e)
+}
 
